4e7772497b2548a39c6969f0170f6b88e6a468ee
haodong191 <hdong215@gmail.com>
2022-12-06 14:40:57 -0500

    Started R with Tidyverse analysis

---
+++ superstore analysis with R.r
@@ -0,0 +1,42 @@
+library(readr)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+
+
+setwd("C:/Users/Hao/Desktop/Data Analysis/superstore-project/Resources") 
+
+data = read_csv("Superstore_final.csv")
+
+group_cateogry_df = data %>% group_by(category)  %>%
+  summarise(total_sales = sum(sales),
+            total_profits = sum(profit),
+            .groups = 'drop')
+
+group_subcateogry_df = data %>% group_by(sub_category)  %>%
+  summarise(total_sales = sum(sales),
+            total_profits = sum(profit),
+            .groups = 'drop')
+
+group_customer_df = data %>% group_by(customer_name)  %>%
+  summarise(total_sales = sum(sales),
+            total_profits = sum(profit),
+            .groups = 'drop')
+
+
+category_bar<-ggplot(data=group_cateogry_df, aes(x=category, y=total_sales)) +
+  geom_bar(stat="identity", width=0.5, fill="steelblue")+
+  geom_text(aes(label=total_sales), vjust=1.6, color="white", size=3.5)+
+  theme_minimal()
+category_bar
+
+subcateogry_bar<-ggplot(data=group_subcateogry_df, aes(x=sub_category, y=total_sales, fill=sub_category)) +
+  geom_bar(stat="identity")
+  geom_text(aes(label=total_sales), vjust=-0.3, size=3.5)+
+  theme_minimal()
+subcateogry_bar
+
+
+customer_bar<-ggplot(data=group_customer_df, aes(x=total_profits, y=total_sales, fill=customer_name)) +
+  geom_bar(stat="identity", position=position_dodge())
+customer_bar
